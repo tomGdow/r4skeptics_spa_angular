5ce192315492980fa26ea7672e589fb3e6e023a8
tomgdow <thomasgdowling@gmail.com>
2015-06-15 15:30:29 +0100

    removed 'skip-before-filter' authenticity token reg controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 	# before_filter :configure_sign_up_params, only: [:create]
 	#before_filter :configure_account_update_params, only: [:update]
-	skip_before_filter  :verify_authenticity_token
+	#	skip_before_filter  :verify_authenticity_token
 	respond_to :html, :js
 	def new
 		super
