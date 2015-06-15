fa7f1560b6b2ad27f7034986a728b04f1f1ff9e5
tomgdow <thomasgdowling@gmail.com>
2015-06-15 19:05:45 +0100

    added further debug messages and precompiled assets before deployed

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	# before_filter :configure_sign_up_params, only: [:create]
 	#before_filter :configure_account_update_params, only: [:update]
 	before_filter :authenticate_user!
-		skip_before_filter  :verify_authenticity_token
+	skip_before_filter  :verify_authenticity_token
 	respond_to :html, :js
 	def new
 		super
