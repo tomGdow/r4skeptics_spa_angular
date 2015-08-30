755c8b033be3f3ba4a65e47da44dbdc1d882a347
tomgdow <thomasgdowling@gmail.com>
2015-08-30 08:26:23 +0100

    clobbered assets. Added config.assets.version = '1.1'

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module R4skeptics
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.version = '1.1'
   end
 end
